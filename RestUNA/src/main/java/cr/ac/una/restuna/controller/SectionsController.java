207160f36d866c2748438ef7b7e2efe8b71ed1b4
aaronvalverde <aaronvalverdeq@gmail.com>
2025-10-29 12:34:45 -0600

    feat(ui): update sections view + controller
    - update sections layout.
    - add logic to manage table creation, drag & dropped function & edit mode buttons.

--- RestUNA/src/main/java/cr/ac/una/restuna/controller/SectionsController.java
+++ RestUNA/src/main/java/cr/ac/una/restuna/controller/SectionsController.java
@@ -6,13 +6,20 @@ package cr.ac.una.restuna.controller;
 
 import io.github.palexdev.materialfx.controls.MFXButton;
 import io.github.palexdev.materialfx.controls.MFXComboBox;
+import io.github.palexdev.materialfx.controls.MFXScrollPane;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 /**
  * FXML Controller class
@@ -22,50 +29,71 @@ import javafx.scene.layout.HBox;
 public class SectionsController extends Controller implements Initializable {
 
     @FXML
-    private MFXComboBox<?> cmbSectionChooser;
-    @FXML
-    private MFXButton btnEdit;
-    @FXML
-    private MFXButton btnDelete;
-    @FXML
-    private AnchorPane tablesRoot;
-    @FXML
     private MFXButton btnAddTable;
     @FXML
-    private MFXButton btnDeleteTable;
+    private MFXButton btnDeleteTable; //habilitar este botón unicamente cuando se detecte un click en alguna de las mesas (estando en modo edición).
     @FXML
     private MFXButton btnExitEditMode;
     @FXML
-    private HBox adminBox;
+    private MFXButton btnEditMode;
     @FXML
-    private HBox editBox;
-
-    private Boolean editMode = false;
+    private MFXScrollPane sectionsRoot;
+    @FXML
+    private VBox sectionsContainer; //agregar todas las secciones en este contenedor.
+    @FXML
+    private MFXButton btnExit;
+    @FXML
+    private AnchorPane sectionPane;
+    @FXML
+    private VBox editModeBox;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        setBoxes(editMode);
+        sectionsContainer.prefHeightProperty().bind(sectionsRoot.heightProperty());
+        sectionsContainer.prefWidthProperty().bind(sectionsRoot.widthProperty());
+        onEditMode(false);
     }
-    
+
     @Override
     public void initialize() {
     }
 
     @FXML
-    private void onActionBtnEdit(ActionEvent event) {
-        editMode = true;
-        setBoxes(editMode);
-    }
-
-    @FXML
-    private void onActionBtnDelete(ActionEvent event) {
-    }
-
-    @FXML
     private void onActionBtnAddTable(ActionEvent event) {
+        MFXButton table = new MFXButton();
+        //String path = " "; //cargar el path del diseño de la mesa para dicho salón.
+        //table.setGraphic(new ImageView(new Image(path)));
+        table.setText("1"); //se podría colocar el número de mesa, pero es algo opcional.
+        table.setPrefHeight(80);
+        table.setPrefWidth(80);
+
+        table.setOnDragDetected(e -> {
+            Dragboard db = table.startDragAndDrop(TransferMode.MOVE);
+            ClipboardContent content = new ClipboardContent();
+            content.putString("table");
+            db.setContent(content);
+            e.consume();
+        });
+
+        sectionPane.setOnDragOver(e -> {
+            if (e.getGestureSource() != sectionPane) { //esta línea verifica que no se este haciendo un drag de sí mismo.
+                e.acceptTransferModes(TransferMode.MOVE);
+            }
+            e.consume();
+        });
+
+        sectionPane.setOnDragDropped(e -> {
+            double x = e.getX();
+            double y = e.getY();
+            table.relocate(x, y); //settea la mesa en la nueva posición.
+            e.setDropCompleted(true);
+            e.consume();
+        });
+
+        sectionPane.getChildren().add(table);
     }
 
     @FXML
@@ -74,21 +102,30 @@ public class SectionsController extends Controller implements Initializable {
 
     @FXML
     private void onActionBtnExitEditMode(ActionEvent event) {
-        editMode = false;
-        setBoxes(editMode);
+        onEditMode(false);
     }
 
-    private void setBoxes(Boolean editMode) {
-        if (editMode) {
-            adminBox.setVisible(false);
-            adminBox.setManaged(false);
-            editBox.setVisible(true);
-            editBox.setManaged(true);
-            return;
-        }
-        adminBox.setVisible(true);
-        adminBox.setManaged(true);
-        editBox.setVisible(false);
-        editBox.setManaged(false);
+    @FXML
+    private void onActionBtnEditMode(ActionEvent event) {
+        onEditMode(true);
+        //confirmación de guardar cambios (si los hay).
+    }
+
+    @FXML
+    private void onActionBtnExit(ActionEvent event) {
+    }
+
+    private void onEditMode(Boolean editMode) {
+        editModeBox.setVisible(editMode);
+        editModeBox.setVisible(editMode);
+        btnExitEditMode.setVisible(editMode);
+        btnExitEditMode.setManaged(editMode);
+        btnEditMode.setVisible(!editMode);
+        btnEditMode.setManaged(!editMode);
+    }
+
+    private void loadSections() {
+        //cargar todos los salones(secciones) en forma de mfxbuttons, setteando button.setText(...) con el nombre de dicha salón.
+        //agregarlos a sectionsContainer.
     }
 }
