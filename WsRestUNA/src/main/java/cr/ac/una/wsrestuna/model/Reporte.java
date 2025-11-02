c21c33c7990ee6d796e08146b66f19bb019b9d21
Erick Fonseca <erick.fonseca.mata@est.una.ac.cr>
2025-11-02 17:00:17 -0600

    entity and dto

---
+++ WsRestUNA/src/main/java/cr/ac/una/wsrestuna/model/Reporte.java
@@ -0,0 +1,145 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cr.ac.una.wsrestuna.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ *
+ * @author fonse
+ */
+@Entity
+@Table(name = "REPORTE", schema = "RESTUNA")
+@NamedQueries({
+    @NamedQuery(name = "Reporte.findAll", query = "SELECT p FROM Reporte p ORDER BY p.fechaGeneracion DESC"),
+    @NamedQuery(name = "Reporte.findByTipo", query = "SELECT p FROM Reporte p WHERE p.tipoReporte = :tipo ORDER BY p.fechaGeneracion DESC"),
+    @NamedQuery(name = "Reporte.findByFecha", query = "SELECT p FROM Reporte p WHERE p.fechaGeneracion BETWEEN :fechaInicio AND :fechaFin ORDER BY p.fechaGeneracion DESC")
+})
+public class Reporte implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "reporte_seq")
+    @SequenceGenerator(name = "reporte_seq", sequenceName = "SEQ_REPORTE", allocationSize = 1)
+    @Column(name = "ID_REPORTE")
+    private Long idReporte;
+
+    @Column(name = "TIPO_REPORTE", length = 50, nullable = false)
+    private String tipoReporte;
+
+    @Column(name = "NOMBRE_REPORTE", length = 255, nullable = false)
+    private String nombreReporte;
+
+    @Lob
+    @Column(name = "PDF")
+    private byte[] pdf;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "FECHA_GENERADO", nullable = false)
+    private Date fechaGenerado;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ID_USUARIO")
+    private Usuario usuario;
+
+    @Column(name = "PARAMETROS", length = 1000)
+    private String parametros;
+
+    public Reporte() {
+        this.fechaGenerado = new Date();
+    }
+
+    public Reporte(ReporteDto reportDto) {
+        this();
+        this.idReporte = reportDto.getIdReporte();
+        actualizar(reportDto);
+
+    }
+
+    public void actualizar(ReporteDto reportDto) {
+        this.tipoReporte = reportDto.getTipoReporte();
+        this.nombreReporte = reportDto.getNombreReporte();
+        this.parametros = reportDto.getParametros();
+        this.pdf = reportDto.getPdf();
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public Long getIdReporte() {
+        return idReporte;
+    }
+
+    public String getTipoReporte() {
+        return tipoReporte;
+    }
+
+    public String getNombreReporte() {
+        return nombreReporte;
+    }
+
+    public byte[] getPdf() {
+        return pdf;
+    }
+
+    public Date getFechaGenerado() {
+        return fechaGenerado;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public String getParametros() {
+        return parametros;
+    }
+
+    public void setIdReporte(Long idReporte) {
+        this.idReporte = idReporte;
+    }
+
+    public void setTipoReporte(String tipoReporte) {
+        this.tipoReporte = tipoReporte;
+    }
+
+    public void setNombreReporte(String nombreReporte) {
+        this.nombreReporte = nombreReporte;
+    }
+
+    public void setPdf(byte[] pdf) {
+        this.pdf = pdf;
+    }
+
+    public void setFechaGenerado(Date fechaGenerado) {
+        this.fechaGenerado = fechaGenerado;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public void setParametros(String parametros) {
+        this.parametros = parametros;
+    }
+
+}
---
+++ WsRestUNA/src/main/java/cr/ac/una/wsrestuna/model/ReporteDto.java
@@ -0,0 +1,163 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cr.ac.una.wsrestuna.model;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.Date;
+
+/**
+ *
+ * @author fonse
+ */
+@Schema(description = "Reportes para consultas y la generacion ")
+public class ReporteDto {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long idReporte;
+
+    private String tipoReporte;
+
+    private String nombreReporte;
+
+    private byte[] pdf;
+
+    private Date fechaGenerado;
+
+    private Long idUsuario;
+
+    private String parametros;
+
+    private String nombreUsuario;
+
+    private Date fechaInicio;
+
+    private Date fechaFin;
+
+    private Long idCajero;
+
+    private Long idFactura;
+
+    public ReporteDto() {
+    }
+
+    public ReporteDto(Reporte reporte) {
+
+        this.idReporte = reporte.getIdReporte();
+        this.tipoReporte = reporte.getTipoReporte();
+        this.nombreReporte = reporte.getNombreReporte();
+        this.pdf = reporte.getPdf();
+        this.fechaGenerado = reporte.getFechaGenerado();
+        this.parametros = reporte.getParametros();
+
+        if (reporte.getUsuario() != null) {
+            this.idUsuario = reporte.getUsuario().getIdUsuario();
+            this.nombreUsuario = reporte.getUsuario().getNombre();
+        }
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public Long getIdReporte() {
+        return idReporte;
+    }
+
+    public String getTipoReporte() {
+        return tipoReporte;
+    }
+
+    public String getNombreReporte() {
+        return nombreReporte;
+    }
+
+    public byte[] getPdf() {
+        return pdf;
+    }
+
+    public Date getFechaGenerado() {
+        return fechaGenerado;
+    }
+
+    public Long getIdUsuario() {
+        return idUsuario;
+    }
+
+    public String getParametros() {
+        return parametros;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+
+    public Long getIdCajero() {
+        return idCajero;
+    }
+
+    public Long getIdFactura() {
+        return idFactura;
+    }
+
+    public void setIdReporte(Long idReporte) {
+        this.idReporte = idReporte;
+    }
+
+    public void setTipoReporte(String tipoReporte) {
+        this.tipoReporte = tipoReporte;
+    }
+
+    public void setNombreReporte(String nombreReporte) {
+        this.nombreReporte = nombreReporte;
+    }
+
+    public void setPdf(byte[] pdf) {
+        this.pdf = pdf;
+    }
+
+    public void setFechaGenerado(Date fechaGenerado) {
+        this.fechaGenerado = fechaGenerado;
+    }
+
+    public void setIdUsuario(Long idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public void setParametros(String parametros) {
+        this.parametros = parametros;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public void setIdCajero(Long idCajero) {
+        this.idCajero = idCajero;
+    }
+
+    public void setIdFactura(Long idFactura) {
+        this.idFactura = idFactura;
+    }
+
+    
+    
+}
